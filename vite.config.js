48758d02016e4719df482b41ecdd4f560b23c2b9
Raj <52691633+TechsavyRaj@users.noreply.github.com>
2026-03-02 18:05:36 +0530

    chore: updated proxy
    Formatted import statements and adjusted proxy configuration.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import {fileURLToPath, URL} from 'node:url'
+import { fileURLToPath, URL } from 'node:url'
 
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -17,8 +17,7 @@ export default defineConfig({
         proxy: {
             '/api': {
                 target: 'http://127.0.0.1:5000',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, ''),
+                changeOrigin: true
             },
         },
     },
